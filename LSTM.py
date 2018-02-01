a2f97dc3b8c4998846963810ab350a8bc4c41bbb
Diego Valenzuela Iturra <diego.valenzuela.i@ing.uchile.cl>
2018-02-01 18:56:51 -0300

    typo

--- LSTM.py
+++ LSTM.py
@@ -472,7 +472,7 @@ model = build_model(len_voc=len(voc),
                     max_len=max_len,
                     embedding_dim=embedding_dim,
                     implementation=implementation,
-                    unroll=unroll):
+                    unroll=unroll)
 
 # Model Summary
 print(model.summary())
