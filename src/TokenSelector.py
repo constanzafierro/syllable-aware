fc7a6a3db1ee745cd52d36fa07601f322387549f
Diego Valenzuela <diego.valenzuela.i@ing.uchile.cl>
2018-02-23 17:56:01 -0300

    perplexity i guess

--- src/TokenSelector.py
+++ src/TokenSelector.py
@@ -8,15 +8,16 @@ from .utils import tokenize_corpus, get_most_frequent
 
 # TODO: Agregar map_punctuation y letters como input a clase processCorpus.py
 
+
 class TokenSelector():
 
     def __init__(self,
-                 final_char=':',
-                 inter_char='-',
-                 sign_to_ignore=[],
-                 word_to_ignore=[],
-                 map_punctuation = {'¿': '<ai>', '?': '<ci>', '.': '<pt>', '\n': '<nl>', ',': '<cm>', '<unk>':'<unk>', ':':'<dc>', ';':'<sc>'},
-                 letters = 'aáeéoóíúiuübcdfghjklmnñopqrstvwxyz'
+                 final_char,
+                 inter_char,
+                 sign_to_ignore,
+                 word_to_ignore,
+                 map_punctuation,
+                 letters
                  ):
 
         self.final_char = final_char
@@ -36,27 +37,32 @@ class TokenSelector():
 
         to_ignore = [i for i in self.punctuation]
         to_ignore = to_ignore + self.sign_to_ignore + self.word_to_ignore
-        self.dict_word, self.dict_syll, self.freq_word, self.freq_syll = tokenize_corpus(path_file, to_ignore = to_ignore)
+
+        self.dict_word, self.dict_syll, self.freq_word, self.freq_syll = tokenize_corpus(path_file, to_ignore=to_ignore)
 
 
     def get_frequent(self, quantity_word, quantity_syll):
 
-        self.words = get_most_frequent(self.freq_word, quantity_word)
+        self.words = get_most_frequent(self.freq_word, quantity_word, to_ignore=[])
+
         # count syllables ignoring most frequent words
-        self.syllables = get_most_frequent(self.freq_syll, quantity_syll, [w for w in self.words])
+        self.syllables = get_most_frequent(self.freq_syll, quantity_syll, to_ignore=[w for w in self.words])
 
 
     def select(self, token, tokens_selected):
 
         if token in self.punctuation:
             tokens_selected.append(self.map_punctuation[token])
+
         elif token in self.dict_word:
             if token in self.words:
                 tokens_selected.append(token + self.final_char)
+
             else:
                 for s in self.dict_word[token]:
                     if s in self.syllables:
                         tokens_selected.append(s)
+
                     else:
                         for c in self.dict_syll[s]:
                             if c in self.characters:
--- src/corpusClass.py
+++ src/corpusClass.py
@@ -15,7 +15,9 @@ class Corpus:
                  final_punc='>',
                  inter_char='-',
                  sign_to_ignore=[],
-                 word_to_ignore=[]
+                 word_to_ignore=[],
+                 map_punctuation={'¿': '<ai>', '?': '<ci>', '.': '<pt>', '\n': '<nl>', ',': '<cm>', '<unk>':'<unk>', ':':'<dc>', ';':'<sc>'},
+                 letters='aáeéoóíúiuübcdfghjklmnñopqrstvwxyz'
                  ):
 
         self.path_to_file = path_to_file
@@ -25,11 +27,16 @@ class Corpus:
         self.inter_char = inter_char
         self.sign_to_ignore = sign_to_ignore
         self.word_to_ignore = word_to_ignore
+        self.map_punctuation = map_punctuation
+        self.letters = letters
+
 
         self.tokenSelector = TokenSelector(final_char = self.final_char,
                                            inter_char = self.inter_char,
                                            sign_to_ignore = self.sign_to_ignore,
-                                           word_to_ignore = self.word_to_ignore
+                                           word_to_ignore = self.word_to_ignore,
+                                           map_punctuation=self.map_punctuation,
+                                           letters=self.letters
                                            )
 
         self.tokenSelector.get_dictionary(self.path_to_file)
@@ -65,7 +72,10 @@ class Corpus:
         
         self.vocabulary = set(self.token_selected)
         self.token_to_index = dict((t, i) for i, t in enumerate(self.vocabulary, 1))
-        self.indice_ends = ending_tokens_index(self.token_to_index, [self.final_char, self.final_punc])
+
+        self.indice_ends = ending_tokens_index(token_to_index = self.token_to_index,
+                                               ends = [self.final_char, self.final_punc]
+                                               )
         self.index_to_token = dict((self.token_to_index[t], t) for t in self.vocabulary)
         self.ind_corpus = [self.token_to_index[token] for token in self.token_selected] # corpus as indexes
         self.vocabulary_as_index = set(self.ind_corpus) # vocabualry as index
@@ -77,12 +87,14 @@ class Corpus:
         self.vocabulary_eval = set(self.eval_set) # indexes
 
 
-    def split_train_eval(self, val_percentage, token_split, min_len = 0):
+    def split_train_eval(self, val_percentage, token_split, min_len=0):
 
         self.vocabulary = set(self.token_selected)
         self.token_to_index = dict((t, i) for i, t in enumerate(self.vocabulary, 1))
 
-        self.indice_ends = ending_tokens_index(self.token_to_index, [self.final_char, self.final_punc])
+        self.indice_ends = ending_tokens_index(token_to_index = self.token_to_index,
+                                               ends =[self.final_char, self.final_punc]
+                                               )
 
         self.index_to_token = dict((self.token_to_index[t], t) for t in self.vocabulary)
         self.ind_corpus = [self.token_to_index[token] for token in self.token_selected] # corpus as indexes
@@ -127,7 +139,7 @@ class Corpus:
 
     def get_generators(self, batch_size):
 
-        train_generator = GeneralGenerator(batch_size,
+        train_generator = GeneralGenerator(batch_size = batch_size,
                                            ind_tokens = self.train_set,
                                            vocabulary = self.vocabulary,
                                            max_len = self.lprime,
@@ -135,7 +147,7 @@ class Corpus:
                                            count_to_split = -1
                                            )
 
-        eval_generator = GeneralGenerator(batch_size,
+        eval_generator = GeneralGenerator(batch_size = batch_size,
                                           ind_tokens = self.eval_set,
                                           vocabulary = self.vocabulary,
                                           max_len = self.lprime,
