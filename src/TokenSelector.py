4adaf4b2ab87e4c5ed03e8e55078f6f087554f3e
Diego Valenzuela <diego.valenzuela.i@ing.uchile.cl>
2018-02-23 04:51:18 -0300

    .

--- src/TokenSelector.py
+++ src/TokenSelector.py
@@ -3,7 +3,13 @@ from utils import tokenize_corpus, get_most_frequent
 
 class TokenSelector():
 
-    def __init__(self, final_char=':', inter_char='-', sign_to_ignore=[], word_to_ignore=[]):
+    def __init__(self,
+                 final_char=':',
+                 inter_char='-',
+                 sign_to_ignore=[],
+                 word_to_ignore=[]
+                 ):
+
         self.final_char = final_char
         self.inter_char = inter_char
         self.sign_to_ignore = sign_to_ignore
@@ -16,23 +22,27 @@ class TokenSelector():
         # Character
         letters = 'aáeéoóíúiuübcdfghjklmnñopqrstvwxyz'
         letters += letters.upper()
+
         self.accepted_chars = set(letters)
         self.characters = self.accepted_chars
 
 
     def get_dictionary(self, path_file):
+
         to_ignore = [i for i in self.punctuation]
         to_ignore = to_ignore + self.sign_to_ignore + self.word_to_ignore
         self.dict_word, self.dict_syll, self.freq_word, self.freq_syll = tokenize_corpus(path_file, to_ignore = to_ignore)
 
 
     def get_frequent(self, quantity_word, quantity_syll):
+
         self.words = get_most_frequent(self.freq_word, quantity_word)
         # count syllables ignoring most frequent words
         self.syllables = get_most_frequent(self.freq_syll, quantity_syll, [w for w in self.words])
 
 
     def select(self, token, tokens_selected):
+
         if token in self.punctuation:
             tokens_selected.append(self.map_punctuation[token])
         elif token in self.dict_word:
@@ -45,4 +55,5 @@ class TokenSelector():
                     else:
                         for c in self.dict_syll[s]:
                             tokens_selected.append(c)
+
         return tokens_selected
